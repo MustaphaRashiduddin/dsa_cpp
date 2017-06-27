ad833934983000f112f527f17ced76df3de1fd0b
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2017-06-27 09:14:46 +0100

    player output more readable

--- utilities/player.hpp
+++ utilities/player.hpp
@@ -10,7 +10,7 @@ struct player {
         int score;
 
         player(int id, std::string name, int score) : id{id}, name{name}, score{score} { }
-        friend std::ostream& operator<< (std::ostream& out, const player& p) { return out << p.id << " | " << p.name << " | " << p.score << "."; }
+        friend std::ostream& operator<< (std::ostream& out, const player& p) { return out << p.id << "; " << p.name << "; " << p.score << " |"; }
         int getid() { return id; }
 };
 
