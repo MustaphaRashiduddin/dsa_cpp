a5c2d4769d06b8f2e6550baf75f1e826de9bf5d2
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2017-07-17 23:09:44 +0100

    prelim split func, vector read debug func

--- mergesort/main.cpp
+++ mergesort/main.cpp
@@ -4,14 +4,20 @@
 using std::vector;
 using std::cout;
 using std::endl;
+using std::string;
 
 void merge_sort(vector<int>&, vector<int>&);
+void split(vector<int>&);
+void pvec(string&&, vector<int>&);
 
 int main (int argc, char **argv)
 {
         vector<int> arr1 = {9, 39, 55, 78, 86};
         vector<int> arr2 = {33, 45, 76, 78, 80};
-        merge_sort(arr1, arr2);
+        pvec("arr2", arr2);
+        // vector<int> arr2 = {33, 45, 76, 78};
+        // merge_sort(arr1, arr2);
+        split(arr2);
         return 0;
 }
 
@@ -26,6 +32,43 @@ void merge_sort(vector<int>& arr1, vector<int>& arr2)
                 else 
                         merged.push_back(arr2[j++]);
 
-        for (int i=0; i<merged.size(); i++)
-                cout << merged[i] << " ";
+        pvec("arr1", arr1);
+        pvec("arr2", arr2);
+        pvec("merged", merged);
+}
+
+void split(vector<int> &v)
+{
+        vector<int> a;
+        vector<int> b;
+        int inc = 0;
+        for (int i=0; i<v.size()/2; i++) {
+                a.push_back(v[i]);
+                inc = i;
+        }
+        for (int i=inc+1; i<v.size(); i++)
+                b.push_back(v[i]);
+
+        // printing stuff
+        if (a.size() != 1)
+                split(a);
+        else {
+                pvec("a", a);
+                pvec("b", b);
+        }
+        if (b.size() != 1)
+                split(b);
+        else {
+                // pvec("b", b);
+                // return;
+        }
+        
+}
+
+void pvec(string&& lbl, vector<int>& v)
+{
+        cout << lbl << ": ";
+        for (int i=0; i<v.size(); i++)
+                cout << v[i] << " ";
+        cout << endl;
 }
