db6fcb296cce0a20bb54fbf5aa76d9d7ae876ace
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2017-07-21 00:35:40 +0100

    merge_sort list works for even elements only

--- mergesort/main.cpp
+++ mergesort/main.cpp
@@ -1,27 +1,45 @@
 #include <vector>
+#include <list>
 #include <iostream>
 
 using std::vector;
+using std::list;
 using std::cout;
 using std::endl;
 using std::string;
 
-void merge_sort(vector<int>&, vector<int>&);
-void split(vector<int>&);
-void pvec(string&&, vector<int>&);
+vector<int> merge_sort(vector<int>&, vector<int>&);
+void merge_sort(list<vector<int>>&);
+void split(list<vector<int>>&, vector<int>&);
+void pvec(string&&, vector<int>);
 
 int main (int argc, char **argv)
 {
-        vector<int> arr1 = {9, 39, 55, 78, 86};
-        vector<int> arr2 = {33, 45, 76, 78, 80};
-        pvec("arr2", arr2);
-        // vector<int> arr2 = {33, 45, 76, 78};
-        // merge_sort(arr1, arr2);
-        split(arr2);
+        /*
+         * vector<int> arr1 = {4, 15, 16, 50};
+         * vector<int> arr2 = {8, 23, 42, 108};
+         */
+        vector<int> v = {108, 15, 50, 4, 8, 42, 23, 16};
+        list<vector<int>> lv;
+        split(lv, v);
+        merge_sort(lv);
+        merge_sort(lv);
+        merge_sort(lv);
+        for (list<vector<int>>::const_iterator i=lv.begin(); i!=lv.end(); i++)
+                pvec("lv", *i);
         return 0;
 }
 
-void merge_sort(vector<int>& arr1, vector<int>& arr2)
+void merge_sort(list<vector<int>>& lv)
+{
+        for (list<vector<int>>::iterator i=lv.begin(); i!=lv.end(); ++i)
+        {
+                *i = merge_sort(*i, *(++i));
+                lv.erase(i); // constant complexity for list
+        }
+}
+
+vector<int> merge_sort(vector<int>& arr1, vector<int>& arr2)
 {
         vector<int> merged;
         int i=0;
@@ -32,12 +50,15 @@ void merge_sort(vector<int>& arr1, vector<int>& arr2)
                 else 
                         merged.push_back(arr2[j++]);
 
-        pvec("arr1", arr1);
-        pvec("arr2", arr2);
-        pvec("merged", merged);
+        /*
+         * pvec("arr1", arr1);
+         * pvec("arr2", arr2);
+         * pvec("merged", merged);
+         */
+        return merged;
 }
 
-void split(vector<int> &v)
+void split(list<vector<int>>& lv, vector<int>& v)
 {
         vector<int> a;
         vector<int> b;
@@ -51,21 +72,20 @@ void split(vector<int> &v)
 
         // printing stuff
         if (a.size() != 1)
-                split(a);
+                split(lv, a);
         else {
-                pvec("a", a);
-                pvec("b", b);
+                // pvec("a", a);
+                lv.push_back(a);
         }
         if (b.size() != 1)
-                split(b);
+                split(lv, b);
         else {
                 // pvec("b", b);
-                // return;
+                lv.push_back(b);
         }
-        
 }
 
-void pvec(string&& lbl, vector<int>& v)
+void pvec(string&& lbl, vector<int> v)
 {
         cout << lbl << ": ";
         for (int i=0; i<v.size(); i++)
