9f9341db0aee760ddde27dbc606d8d5579281a50
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2017-07-30 22:00:39 +0100

    add recursive steps

--- quicksort/main.cpp
+++ quicksort/main.cpp
@@ -9,11 +9,14 @@ void parr(int[], unsigned);
 
 int main(int argc, char **argv)
 {
-        int arr[] = {8, 3, 1, 7, 0, 10, 2};
+        // int arr[] = {8, 3, 1, 7, 0, 10, 2};
+        int arr[] = {108, 15, 50, 4, 8, 42, 23, 16};
         int arr_sz = sizeof arr / sizeof *arr;
         parr(arr, arr_sz);
 
         qsort(arr, 0, arr_sz);
+
+        parr(arr, arr_sz);
 
         return 0;
 }
@@ -25,14 +28,14 @@ void qsort(int arr[] , unsigned l, unsigned r)
                 if (arr[l] > arr[p]) {
                         swap(arr[l], arr[p]);
                         swap(arr[l], arr[p-1]);
-                        p = p-1;
+                        --p;
                 } else {
                         ++l;
                 }
         }
-        parr(arr, r);
-        cout << "l: " << l << endl;
-        cout << "r: " << r << endl;
+
+        if (p != -1) qsort(arr, 0, p);
+        if (l != r) qsort(arr, l, r);
 }
 
 void parr(int arr[], unsigned sz)
