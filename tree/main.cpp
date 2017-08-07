90a71b6d7c551a8ee5d87b3eae9c7828ddce75aa
Mustapha Rashiduddin <saif.rashiduddin@gmail.com>
2017-08-07 22:10:14 +0100

    tree | demonstrating left, right node in main.cpp

--- tree/main.cpp
+++ tree/main.cpp
@@ -4,10 +4,16 @@
 using std::unique_ptr;
 using std::cout;
 using std::endl;
+using std::move;
 
 int main(int argc, char **argv)
 {
         // node(T dat) : dat{new T(dat)}, r{nullptr}, l{nullptr} { }
         node<int> root{node<int>{3}};
-        cout << root << endl;
+        unique_ptr<node<int>> r{new node<int>{5}};
+        root.r = move(r);
+        unique_ptr<node<int>> l{new node<int>{6}};
+        root.l = move(l);
+        cout << root << " " << *root.r << " " << *root.l << endl;
+        return 0;
 }
